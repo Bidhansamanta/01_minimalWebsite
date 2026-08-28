48faae974444b952451626a0a47126c567f8943e
Bidhan Samanta <bidhansamanta2004@gmail.com>
2026-08-28 15:39:46 +0530

    Add React Rount Dom  from LINk

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,45 +1,47 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <header className="bottom-0 left-0 z-50 h-20 w-full rounded-2xl bg-blue-300 sticky">
       <ul className="flex h-full items-center justify-around px-3 sm:justify-center sm:gap-12 sm:px-4 md:gap-24 lg:gap-40 xl:gap-60">
         <li className="cursor-pointer text-xl font-semibold text-black">
-          <a
-            href="/"
+          <Link
+            to="/"
             className="flex flex-col items-center justify-center gap-1"
           >
             <i className="fa-solid fa-house text-xl text-black"></i>
             <div className="text-sm text-black">HOME</div>
-          </a>
+          </Link>
         </li>
 
         <li className="cursor-pointer text-xl font-semibold text-black">
-          <a
-            href="/explore"
+          <Link
+            to="/explore"
             className="flex flex-col items-center justify-center gap-1"
           >
             <i className="fa-solid fa-compass text-xl text-black"></i>
             <div className="text-sm text-black">EXPLORE</div>
-          </a>
+          </Link>
         </li>
 
         <li className="cursor-pointer text-xl font-semibold text-black ">
-          <a
-            href="/collections"
+          <Link
+            to="/collections"
             className="flex flex-col items-center justify-center gap-1"
           >
             <i className="fa-solid fa-vault text-xl text-black"></i>
             <div className="text-sm text-black">VAULT</div>
-          </a>
+          </Link>
         </li>
 
         <li className="cursor-pointer text-xl font-semibold text-black ">
-          <a
-            href="/account"
+          <Link
+            to="/account"
             className="flex flex-col items-center justify-center gap-1"
           >
             <i className="fa-solid fa-user text-xl text-black"></i>
             <div className="text-sm text-black">ACCOUNT</div>
-          </a>
+          </Link>
         </li>
       </ul>
     </header>
